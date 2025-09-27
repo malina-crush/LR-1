425cdf6d9aa58501db22f881e124500bd5bc0ff4
Stemdoo <stemdoo13@gmail.com>
2025-09-27 10:33:28 +0300

    commit

---
+++ номер 1.py
@@ -0,0 +1,14 @@
+int_ = 26
+float_ = 1.6
+summ = int_ + float_
+raz = int_ - float_
+pr = int_ * float_ 
+del_ = int_ / float_ 
+print('Сумма', summ)
+print('Разность', raz)
+print('Произведение', pr)
+print('Деление', del_, 'Тип резуьтата деления',type(del_))
+r = 5 
+pi = 3.14159
+ploshchad = pi * r ** 2
+print('Площадь круга с радиусом 5 равна', round(ploshchad,2))
